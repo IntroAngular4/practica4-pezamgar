0f139c1c1905edff667015ef7bda4eaa0a754382
Pere Zamora <pezamgar@gmail.com>
2019-11-21 19:23:22 +0100

    Add filter projects

--- src/app/projects/filter-projects-form/filter-projects-form.component.ts
+++ src/app/projects/filter-projects-form/filter-projects-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-filter-projects-form',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterProjectsFormComponent implements OnInit {
 
-  constructor() { }
+  public searchProject: string;
+  @Output()
+  change: EventEmitter<string> = new EventEmitter<string>();
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  dataChanged(event) {
+    this.change.emit(this.searchProject);
+  }
 }
